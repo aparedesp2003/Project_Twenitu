055fea96414aadf5f798696de0d214514d0d4b08
Andres Paredes <andres.pp2003@gmail.com>
2025-09-30 18:33:25 -0400

    Update navbar with logo and scroll-to-top feature

--- src/Components/LandingPage/NavBar.jsx
+++ src/Components/LandingPage/NavBar.jsx
@@ -1,24 +1,37 @@
 import React from "react";
 import "./NavBar.css";
+import logo from "../Images/Logo_PRINCIPAL-OFF-WHITE.png";
 
-function NavBar({ onLogin, onSignUp }) {
+const NavBar = ({ onLogin, onSignUp }) => {
+  // scroll to top when logo or name is clicked
+  const handleLogoClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <nav className="navbar">
-      <div className="navbar-logo">TWENITU</div>
-      <ul className="navbar-links">
-        <li><a href="#banner">Home</a></li>
+      {/* Left: Logo + brand */}
+      <div className="navbar-logo" onClick={handleLogoClick} style={{ cursor: "pointer" }}>
+        <img src={logo} alt="Twenitu Logo" className="logo-img" />
+        <span className="company-name">Twenitu</span>
+      </div>
+
+      {/* Center: Navigation links */}
+      <ul className="nav-links">
         <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
         <li><a href="#products">Products</a></li>
         <li><a href="#projects">Projects</a></li>
-        <li><a href="#services">Services</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
-      <div className="navbar-buttons">
-        <button className="login-btn" onClick={onLogin}>Log In</button>
+
+      {/* Right: Auth buttons */}
+      <div className="nav-auth">
+        <button className="login-btn" onClick={onLogin}>Login</button>
         <button className="signup-btn" onClick={onSignUp}>Sign Up</button>
       </div>
     </nav>
   );
-}
+};
 
 export default NavBar;
